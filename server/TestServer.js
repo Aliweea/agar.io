d5ba6bff5ca182b2568f3e83be6fd1892ccc911b
KANXUAN-PC\xuan <kanxuan1996@gmail.com>
2017-05-23 17:32:47 +0800

    添加

--- server/TestServer.js
+++ server/TestServer.js
@@ -25,39 +25,44 @@ function randomPosition(radius) {
 
 
 var defaultUserMass = 30;
-var defaultUser = {
-    id: -1,
-    name: "",
-    position: {
-        x: 0,
-        y: 0
-    },
-    radius:0,
-    allMass: defaultUserMass,
-    color: 0,
-    target: {
-        x: 0,
-        y: 0
-    },
-    cells: [{
+
+function getDefaultUser() {
+    return {
+        id: -1,
+        name: "",
+        position: randomPosition(defaultUserMass),
+        allMass: defaultUserMass,
+        color: Math.round(Math.random() * 360),
+        target: {
+            x: 0,
+            y: 0
+        },
+        cells: [{
             position: {
                 x: 0,
                 y: 0
             },
             color: 0,
-            radius: 0,
-            mass: 0,
+            radius: massToRadius(defaultUserMass),
+            mass: defaultUserMass,
         }]
-};
+    };
+}
+
+var users = [];
+var sockets = {};
 
 io.on("connection", function (socket) {
     console.log("Somebody connect!");
-
+    sockets[socket.id] = socket;
 
     socket.on("gotit", function (name) {
-        var connectUser = {};
-        
-
+        var connectUser = getDefaultUser();
+        connectUser.name = name;
+        connectUser.cells.position.x = connectUser.position.x;
+        connectUser.cells.position.y = connectUser.position.y;
+        connectUser.cells.color = connectUser.color;
+        users.push(connectUser)
     });
     
     socket.on("0", function (target) {
